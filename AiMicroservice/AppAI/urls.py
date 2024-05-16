be25d3e4f60e17a73ca716bc124b001d3dc0aa5a
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-05-17 01:49:11 +0200

    adding todo's as a test

--- AiMicroservice/AppAI/urls.py
+++ AiMicroservice/AppAI/urls.py
@@ -1,1 +1,9 @@
-urlpatterns = []
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('todo/test', views.getRoutes, name='routes'),
+    path('todo/products', views.getProducts, name='products'),
+    path('todo/products/<str:pk>', views.getProductsById, name='products'),
+]

73dcab58be1aa485ffcfdd084e05f6fb8b830127
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-05-21 19:21:48 +0200

    remove todos  and todo urls

--- AiMicroservice/AppAI/urls.py
+++ AiMicroservice/AppAI/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     path('todo/test', views.getRoutes, name='routes'),
-    path('todo/products', views.getProducts, name='products'),
-    path('todo/products/<str:pk>', views.getProductsById, name='products'),
+    path('todo/test-auth', views.getProducts, name='products'),
 ]

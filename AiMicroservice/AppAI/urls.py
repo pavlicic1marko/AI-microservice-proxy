8451ebabae7118d36e96996ec2f4daeee454d760
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-05-23 20:39:09 +0200

    adding delete prompt endpoint

--- AiMicroservice/AppAI/urls.py
+++ AiMicroservice/AppAI/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('todo/test', views.getRoutes, name='routes'),
     path('todo/test-auth', views.getProducts, name='products'),
     path('prompts/<str:pk>', views.getPrompts, name='promts-userId'),
+    path('prompts/delete/<str:pk>', views.getPrompts, name='promts-delete'),
 
 ]
--- AiMicroservice/AppAI/views.py
+++ AiMicroservice/AppAI/views.py
@@ -1,8 +1,11 @@
+import django
+from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from AppAI.models import Prompt
 from AppAI.serializer import PromptSerializer
+import django.core.exceptions
 
 # Create your views here.
 
@@ -26,3 +29,19 @@ def getPrompts(request,pk):
     promts_for_user = Prompt.objects.filter(user_id=user_id)
     serializer = PromptSerializer(promts_for_user, many=True)
     return Response(serializer.data)
+
+@api_view(['DELETE'])
+# @permission_classes([IsAdminUser])
+def deletePromptById(request, pk):
+    prompt_id = pk
+    try:
+        prompt = Prompt.objects.filter(_id=prompt_id)
+        prompt.delete()
+        return Response('prompt is deleted', status=200)
+
+    except django.core.exceptions.ObjectDoesNotExist:
+        return Response('prompt does not exist', status=status.HTTP_404_NOT_FOUND)
+
+    except Exception as e:
+        return Response(e, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+

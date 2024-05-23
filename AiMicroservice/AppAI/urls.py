8182b1d3aa86068524714cd0846994ddca00854f
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-05-23 14:43:21 +0200

    add url for prompts

--- AiMicroservice/AppAI/urls.py
+++ AiMicroservice/AppAI/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('todo/test', views.getRoutes, name='routes'),
     path('todo/test-auth', views.getProducts, name='products'),
+    path('prompts/<str:pk>', views.getPrompts, name='promts-userId'),
+
 ]
--- AiMicroservice/AppAI/views.py
+++ AiMicroservice/AppAI/views.py
@@ -1,6 +1,8 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from AppAI.models import Prompt
+from AppAI.serializer import PromptSerializer
 
 # Create your views here.
 
@@ -14,3 +16,12 @@ def getRoutes(request):
 @permission_classes([IsAuthenticated])
 def getProducts(request):
     return Response({'test-key':'test123'})
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getPrompts():
+    user_id = 1
+    promts_for_user = Prompt.objects.filter(user_id=user_id)
+    serializer = PromptSerializer(promts_for_user, many=True)
+    return Response(serializer.data)
